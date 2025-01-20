b342c2075b272aab7fb439cdc18e6dc85644f0be
Muhamad Iksan <muhamadiksan351@gmail.com>
2025-01-20 20:47:25 +0700

    feat: add api

---
+++ app/Http/Resources/Api/ViewBookingResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ViewBookingResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'phone_number' => $this->phone_number,
+            'booking_trx_id' => $this->booking_trx_id,
+            'is_paid' => $this->duration,
+            'total_amount' => $this->amount,
+            'started_at' => $this->started_at,
+            'ended_at' => $this->ended_at,
+            'officeSpace' => new OfficeSpaceResource($this->whenLoaded('officeSpace'))
+        ];
+    }
+}
